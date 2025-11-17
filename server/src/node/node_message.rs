308aac1451a3d2a3ad704491afc3777acc9f5bc8
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-11-17 11:36:58 -0300

    refactor(node-messages): *raft protocol* messages

--- server/src/node/node_message.rs
+++ server/src/node/node_message.rs
@@ -1,32 +1,48 @@
 use super::operation::Operation;
 use crate::errors::AppError;
 
+/// Messages sent between nodes.
 #[derive(Debug, PartialEq)]
 pub enum NodeMessage {
-    Error,
-    Accept(Operation),
-    /* estos tipos de mensaje están para mostrar el
-     * esqueleto, no deberían llevar todos Operation
-     * adentro
-     ***/
-    Learn(Operation),
-    Commit(Operation),
-    Finished(Operation),
+    /// Operation request sent by the client server (station).
+    ///
+    /// This message is only handled by the coordinator. In case a replica receives it, it resends
+    /// it to the coordinator.
+    Request { op: Operation },
+
+    /// Log message sent by coordinator to the replicas.
+    ///
+    /// The optional `u32` is a placeholder for a commit order. Replicas commit the operation on
+    /// their logs with the `commit: u32` id.
+    Log { op: Operation },
+
+    /// Acknowledgement reply sent by a replica after receiving a valid `Log` message from the
+    /// coordinator.
+    ACK { id: u32 },
 }
 
-// 1. coord -> replicas: accept
-// 2. replicas -> coord: learn
-// 3. coord -> replicas: commit
-// 4. replicas -> coord: finished
+use NodeMessage::*;
 
 impl TryFrom<Vec<u8>> for NodeMessage {
     type Error = AppError;
 
     fn try_from(payload: Vec<u8>) -> Result<Self, AppError> {
         match payload[0] {
-            0x01 => Ok(NodeMessage::Accept(Operation::try_from(&payload[1..])?)),
+            0u8 => Ok(Request {
+                op: payload[1..].try_into()?,
+            }),
+            1u8 => Ok(Log {
+                op: payload[5..].try_into()?,
+            }),
+            2u8 => Ok(ACK {
+                id: u32::from_ne_bytes(payload[1..5].try_into().map_err(|e| {
+                    AppError::InvalidData {
+                        details: format!("not enough bytes to deserialize ACK: {e}"),
+                    }
+                })?),
+            }),
             _ => Err(AppError::InvalidData {
-                details: ("error deserializing node message payload".to_string()),
+                details: ("unknown node message type".to_string()),
             }),
         }
     }
@@ -35,13 +51,24 @@ impl TryFrom<Vec<u8>> for NodeMessage {
 impl From<NodeMessage> for Vec<u8> {
     fn from(msg: NodeMessage) -> Self {
         match msg {
-            NodeMessage::Accept(op) => {
-                let mut srl: Vec<u8> = [0x01].to_vec();
+            NodeMessage::Request { op } => {
+                let mut srl: Vec<u8> = vec![0u8];
                 let op_srl: Vec<u8> = op.into();
                 srl.extend(op_srl);
                 srl
             }
-            _ => [].to_vec(),
+            NodeMessage::Log { op } => {
+                let mut srl = vec![1u8];
+                let op_srl: Vec<u8> = op.into();
+                srl.extend(op_srl);
+                srl
+            }
+            NodeMessage::ACK { id } => {
+                let mut srl = vec![2u8];
+                let id_srl = id.to_be_bytes().to_vec();
+                srl.extend(id_srl);
+                srl
+            }
         }
     }
 }
