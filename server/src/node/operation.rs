f032be99cbfdf500b2379f7db35fa01a544868de
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-11-17 17:47:15 -0300

    refactor(server-operation): pump op id from u8 to u32

--- server/src/node/operation.rs
+++ server/src/node/operation.rs
@@ -2,20 +2,25 @@ use crate::errors::AppError;
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Operation {
-    pub id: u8,
+    pub id: u32,
 }
 
 impl TryFrom<&[u8]> for Operation {
     type Error = AppError;
 
     fn try_from(payload: &[u8]) -> Result<Self, AppError> {
-        // TODO: check validity of payload
-        Ok(Operation { id: payload[0] })
+        Ok(Operation {
+            id: u32::from_ne_bytes(payload[0..5].try_into().map_err(|e| {
+                AppError::InvalidData {
+                    details: format!("not enough bytes to deserialize operation: {e}"),
+                }
+            })?),
+        })
     }
 }
 
 impl From<Operation> for Vec<u8> {
     fn from(op: Operation) -> Self {
-        vec![op.id]
+        op.id.to_be_bytes().to_vec()
     }
 }
